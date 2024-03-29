69bbcb1b042c2ae472f9950b4e9bdbe4570bc155
MJordanBecode <masyjordan@gmail.com>
2024-03-29 13:39:29 +0100

    Exercise 2.6

---
+++ 02.Structure/Exercise-2.6/app.js
@@ -0,0 +1,26 @@
+let number;
+
+
+number= prompt("Choose a number between 1 and 7 : ");
+
+if(number == 1){
+    alert("Monday");
+}
+else if(number == 2){
+    alert("Tuesday");
+}
+else if(number == 3){
+    alert("Wednesday");
+}
+else if(number == 4){
+    alert("Thurday");
+}
+else if(number == 5){
+    alert("Friday");
+}
+else if(number == 6){
+    alert("Saturday");
+}
+else if(number == 7){
+    alert("Sunday");
+}
