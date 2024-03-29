87bd48c6d110c799688ce943b69da2fe42e6269d
MJordanBecode <masyjordan@gmail.com>
2024-03-29 10:07:03 +0100

    nombre de babge

---
+++ 01.intro/1.1/app.js
@@ -0,0 +1,13 @@
+console.log("hello");
+let age = 23; //I'm 23
+age = 36;// I get older
+age = 18; //Miraculously I'm getting younger
+console.log(age);
+
+let htmlBadges = new Number('5');
+
+let cssBadges = new Number('7');
+
+let totalBadges = htmlBadges + cssBadges;
+
+console.log('Woaw, you have ' + totalBadges + "!");
