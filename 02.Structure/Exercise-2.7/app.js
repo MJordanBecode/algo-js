eef04bdba1543d56835051bd7838d323cb2280b0
MJordanBecode <masyjordan@gmail.com>
2024-03-29 13:45:14 +0100

    Exercise 2.7

---
+++ 02.Structure/Exercise-2.7/app.js
@@ -0,0 +1,10 @@
+let number, number2,mult;
+
+number = prompt("Enter a number : ",number);
+number2 = prompt("enter a multiplicateur",number2);
+
+mult = number * number2;
+
+for(var i=1;i <= mult;i++){
+    console.log(i);
+}
