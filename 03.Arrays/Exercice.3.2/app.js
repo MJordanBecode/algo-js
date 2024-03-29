581a8d59f1adb3d2d2eb809eab20eef1da72aff2
MJordanBecode <masyjordan@gmail.com>
2024-03-29 15:45:24 +0100

    Exercice 3.2

--- 03.Arrays/Exercice.3.2/app.js
+++ 03.Arrays/Exercice.3.2/app.js
@@ -1,4 +1,15 @@
 let arr=[1, 2, 3, 4, 5], arr2=[100, 101, 102]; sum=0; sum2=0;
 
-console.log(arr[2]);
-console.log(arr2[1]);
+for(let i=0;i<arr.length;i++){
+    sum += arr[i];
+    console.log(sum);
+}
+sum = sum / arr.length;
+console.log(sum);
+
+for(let i=0;i<arr2.length;i++){
+    sum2 += arr2[i];
+    console.log(sum2);
+}
+sum2 = sum2 / arr2.length;
+console.log(sum2);
