7dd86ad2d137e142d996250fe9d352ec6ce52ab0
MJordanBecode <masyjordan@gmail.com>
2024-03-29 13:17:27 +0100

    Exercise 2.5

---
+++ 02.Structure/Exercise-2.5/app.js
@@ -0,0 +1,13 @@
+let number; 
+
+
+number=prompt("Insert your favorite number : ",number);
+do{
+    if(number != 42){
+        alert("Are you sur");
+    }
+    number=prompt("Insert your favorite number : ",number);
+
+}while(number != 42)
+
+alert("Good job");
