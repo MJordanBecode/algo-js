a006c82aa5aa0c4fa82d6f55cb6c43569db7484a
MJordanBecode <masyjordan@gmail.com>
2024-03-29 14:04:04 +0100

    Exercice 3.0

---
+++ 03.Arrays/Exercice.3.0/app.js
@@ -0,0 +1,5 @@
+let arr = ["Adrien.C", "Adrien.B", "Angel","Arnaud VA","Denis.L","Dylan","Dzheylyan","Giovanni","Isabelle","Jessica","Julie","Justine.F","Justine.L","Lyne","Jordan","Manu","Maryam","Mohamed","Nataliia","Bahra","Caroline","Dorian","Iliess","Louis","Nathanael","Tom","Youri","Stacy",];
+
+for (let elem of arr) {
+  console.log("Name " + elem);
+}
