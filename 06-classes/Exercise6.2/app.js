f3a86b13cf0a9746291d8c5c0a8e1f6864a4aedf
Masy Jordan <masyjordan@gmail.com>
2024-04-04 16:40:36 +0200

    rectification 6.3

--- 06-classes/Exercise6.2/app.js
+++ 06-classes/Exercise6.2/app.js
@@ -6,9 +6,23 @@ class Rectangle{
         lenght = lenght;   
     }
 
-    collides(otherRectangle){
- 
-
-        return ;
+    collides(otherRectangle) {
+        // Check for collision along x-axis
+        const xCollision = (this.topLeftXPos < otherRectangle.topLeftXPos + otherRectangle.width &&
+                            this.topLeftXPos + this.width > otherRectangle.topLeftXPos);
+        
+        // Check for collision along y-axis
+        const yCollision = (this.topLeftYPos < otherRectangle.topLeftYPos + otherRectangle.length &&
+                            this.topLeftYPos + this.length > otherRectangle.topLeftYPos);
+        
+        // Return true if both x and y collisions occur
+        return xCollision && yCollision;
     }
 }
+// Test cases
+const rect1 = new Rectangle(0, 0, 4, 4);
+const rect2 = new Rectangle(2, 5, 4, 4);
+const rect3 = new Rectangle(5, 5, 2, 2);
+
+console.log(rect1.collides(rect2)); // Should return true, as rect1 and rect2 overlap
+console.log(rect1.collides(rect3)); // Should return false, as rect1 and rect3 do not overlap
